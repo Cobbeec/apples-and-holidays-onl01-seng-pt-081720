878fd7c3a55f75ce3228c5be9363fc0a31a7cffd
Elizabeth Cobb <cobbeec@gmail.com>
2020-08-29 17:37:47 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -24,8 +24,7 @@ def add_supply_to_winter_holidays(holiday_supplies, supply)
    decorations.push(supply)
   end
 end
-
-add_supply_to_winter_holidays(holiday_hash, supply)
+return add_supply_to_winter_holidays(holiday_supplies, supply)
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
