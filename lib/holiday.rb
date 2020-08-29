9fc89f87c43e232106e53a4ef58443cdad3e8d22
Elizabeth Cobb <cobbeec@gmail.com>
2020-08-29 17:10:31 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -3,7 +3,7 @@
 :thanksgiving => ["Turkey"] },:spring => { :memorial_day => ["BBQ"]  } }
 
 def second_supply_for_fourth_of_july(holiday_hash)
- return holiday_hash[:summer][:fourth_of_july[1]]
+ return holiday_hash[:summer][:fourth_of_july]
 end 
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
