661db874876659af102e7fb34217772800cb27df
Elizabeth Cobb <cobbeec@gmail.com>
2020-08-29 17:14:56 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,11 +1,6 @@
-  holiday_hash=
-{ :winter => { :christmas => ["Lights", "Wreath"],:new_years => ["Party Hats"]  }, :summer => {:fourth_of_july => ["Fireworks", "BBQ"] }, :fall => {
-:thanksgiving => ["Turkey"] },:spring => { :memorial_day => ["BBQ"]  } }
-
 def second_supply_for_fourth_of_july(holiday_supplies)
   holiday_supplies[:summer][:fourth_of_july][1]
 end
-
 second_supply_for_fourth_of_july(holiday_supplies)
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
