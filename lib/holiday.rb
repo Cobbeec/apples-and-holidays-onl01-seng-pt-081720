17e0ed2fd0cb2a336d498286525e70123d5d900c
Elizabeth Cobb <cobbeec@gmail.com>
2020-08-29 17:42:22 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -19,12 +19,7 @@ def second_supply_for_fourth_of_july(holiday_supplies)
 end
 return second_supply_for_fourth_of_july(holiday_supplies)
 
-def add_supply_to_winter_holidays(holiday_hash, item)
- holiday_hash[:winter].each do |holiday, decorations|
-   decorations.push(item)
-  end
-end
-return add_supply_to_winter_holidays(holiday_supplies, "Cookies")
+
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
