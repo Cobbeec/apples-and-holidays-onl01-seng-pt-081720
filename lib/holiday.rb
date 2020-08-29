b15060940211e2b6935c0681083459aaf69905f7
Elizabeth Cobb <cobbeec@gmail.com>
2020-08-29 17:49:29 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -47,8 +47,8 @@ def add_new_season_and_holiday_with_supplies(holiday_hash, season, holiday_name,
   holiday_hash
 end
 
-mitzvos = ["seder", "matzah", "hagaddah"]
-add_new_season_and_holiday_with_supplies(holiday_supplies, :aviv, :pesach, mitzvos)
+treats = ["candy", "cake", "scotch"]
+add_new_season_and_holiday_with_supplies(holiday_supplies, :fun, :day, treats)
 
 
 def all_winter_holiday_supplies(holiday_hash)
